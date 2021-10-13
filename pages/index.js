35282e32f453b06fa59f9b97039d758c43e1a4f5
unknown <khushishukla0502@gmail.com>
2021-10-13 13:58:20 +0530

    Display filtered pokemon and autocomplete input

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import { resolveHref } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import useSWR from 'swr'
 
+import useSWR from 'swr'
+import { useState } from 'react';
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
@@ -11,6 +10,7 @@ export const getStaticProps = async () =>{
   const res = await fetch('https://pokeapi.co/api/v2/pokemon');
   const data = await res.json();
   if (!data) {
+//    setPokemon(data)
     return {
       redirect: {
         destination: '/',
@@ -18,32 +18,84 @@ export const getStaticProps = async () =>{
       },
     }
   }
-
-  const pokemonData = data.results.url;
-
   return{
     props: { data }
   }
 }
-export default function Home({ data }) {
 
-  return (
-    <div>
-      <h2>Welcome to the next app</h2>
-      <div>{data.results.map((pokemon)=>{
-        const { data, error } = useSWR(pokemon.url, fetcher) //to get the images
+
+export default function Home({ data }) {
+  const [searchTerm, setSearchTerm]=useState('');
+  const [filteredPokemon, setFilteredPokemon]=useState('');
+
+  const filterPokemon = (textToSearch) =>{
+    const allPokemon=data.results;
+    const newPokemon= allPokemon.filter((pokemon) => {if(pokemon.name.toLowerCase().includes(textToSearch.toLowerCase())) return pokemon} )
+      setFilteredPokemon(newPokemon);
+     
+      // console.log(textToSearch)
+      console.log(filteredPokemon)
+     
+  }
+
+
+  const getImage=(url)=>{    
+    const { data, error } = useSWR(url, fetcher) //to get the images
         if (error) return <div>failed to load</div>
         if (!data) return <div>loading...</div>
-        return (
-          <div>
-            <img src={data.sprites.front_default} alt={data.sprites.front_default} />
-            <h2 key={pokemon.id}>{pokemon.name}</h2>
+        {/* if(!error && data) */}
+        return ( 
+         <img key={url} src={data.sprites.front_default} alt={data.sprites.front_default} />
+        )
+  }
 
-            
-          </div>
-          
-        );
-      })}</div>
+    const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    filterPokemon(searchTerm);
+    // console.log(filteredPokemon);
+    // console.log({searchTerm})
+  }
+ const Display = ({data}) =>{
+   if(!{data}){
+     return <div></div>
+   }
+   return(
+  data
+    .map((pokemon)=>{
+    return ( 
+      <div key={pokemon.name} className="justify-self-center" >
+        {getImage(pokemon.url)}
+        <h2 className="text-gray-900 leading-tight">{pokemon.name}</h2>
+      </div>
+      )
+    })
+  
+   );
+ }
+
+const ShowVal = () => {
+  const pokemonNames = [,"raticate","bulbasaur", "ivysaur", "venusaur","charmander","charmeleon","charizard","squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle","kakuna","beedrill","pidgey","pidgeotto","pidgeot","rattata"];
+  return <div>
+    {pokemonNames.filter((name) => {
+      if(name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return name;
+      
+      }
+    })
+    .map(name=>{
+      return <div className=" w-96 bg-gray-50 shadow	" key={name}>{name}</div>
+    })}
+  </div>
+}
+  return (
+    <div className="m-10 ">
+      <center>
+      <input autoComplete="on" type="text" className="border-2 border-blue-200 h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Search pokemon" onChange={handleSearch} value={searchTerm}/>
+      {searchTerm.length>0 ? <ShowVal /> :null}
+      </center>
+      <div className="grid md:grid-cols-4 grid-cols-3">
+       {searchTerm ? <Display data={filteredPokemon} />: <Display data={data.results} />}
+      </div> 
     </div>
   )
 }
